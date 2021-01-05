0c85c6b4983371a05248132a5819ffc7c65ef2ca
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-05 17:02:04 -0600

    updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
 return(
-    <footer className="bg-gray-200 text-center text-xs p-3">
+    <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0">
         
         &copy; Copyright 2021
 
