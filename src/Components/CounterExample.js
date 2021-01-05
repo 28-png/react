d20a5f0a923828d7019cdfefe2155a0ec8906a87
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-05 12:09:18 -0600

    updated

--- src/Components/CounterExample.js
+++ src/Components/CounterExample.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CounterExample() {
-
+    const [count, setCount] = useState(0)
     return (
         <div>
+           <h1> 
+               
+               {count} 
+               
+            </h1>
+            <h1 onClick={() => setCount( count + 1)}>
+            +
+            </h1>
+            <h1 onClick={() => setCount( count - 1)}>
+            -
+            </h1>
 
         </div>
     )
