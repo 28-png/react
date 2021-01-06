d93b16b3af928794f6d2b21a0d7fd04332f4bfcf
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-06 13:15:52 -0600

    updated

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Navigation() {
+    const [showMenu, setShowMenu] = useState(false)
+
 return(
     <nav className="">
-        
+        <span className="text-xl">
         <FontAwesomeIcon 
              icon={faBars}
         />
+
+        </span>
+        
 
     </nav>
 
