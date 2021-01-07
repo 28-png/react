5089bf43e1a19d3ba55d54b97d3fe03ca1239515
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-07 17:00:16 -0600

    updated

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -16,9 +16,9 @@ function Navigation() {
     })
 
     const menuTransitions = useTransition(showMenu, null, {
-        from: { position: 'absolute', opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
         })
 
 
@@ -49,9 +49,16 @@ return(
         item && <animated.div 
         key={key} 
         style={props}
-        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
         >
-                This is the Menu
+                <span>
+                    The Menu
+                </span>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                </ul>
         </animated.div>
         )
     }
