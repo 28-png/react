ee85e78fb2d61d627c6f45090d39b9ff810562a6
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-09 12:55:16 -0600

    updated

--- src/Views/Products.js
+++ src/Views/Products.js
@@ -5,32 +5,52 @@ import { useParams } from 'react-router'
 function Products() {
 const { id } = useParams()
 const url = `https://5ff7d19c10778b0017042839.mockapi.io/api/v1/PRODUCTS/${id}`
-const [product, setProduct] = useState(null)
+const [product, setProduct] = useState({
+    loading: false,
+    data: null
+})
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null,
+
+        })
         axios.get(url)
     .then(response => {
-        setProduct(response.data)
+        setProduct({
+            loading: false,
+            data: response.data
+        })
 
     })
     }, [url])
+
+    if(product.loading){
+        return(
+            <p>...loading</p>
+        )
+    }
+
+        
+        
     
-    if(product) {
+    if(product.data) {
         return(
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                 <img
-                    src={product.imageUrl[0].imageUrl}
-                    alt={product.name}
+                    src={product.data.imageUrl[0].imageUrl}
+                    alt={product.data.name}
                 />
             </div>
             <div className="text-xl font-bold mb-3">
-                {product.price}
+                {product.data.price}
             </div>
             <div>
-                {product.description}
+                {product.data.description}
             </div>
             </div>
            
