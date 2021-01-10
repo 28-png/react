0d8f549b538d0fd66fcab85351dd36e3b79e4fae
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-10 14:19:48 -0600

    updated

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import HelloWorld from '../Components/HelloWorld';
 function Home() {
+    let content = null
     return(
         <div>
-            <h1 className="font-bold text-2xl mb-3" >This is the Home Page</h1>
+            <h1 className="font-bold text-2xl mb-3" >Best Sellers</h1>
 
-            <HelloWorld name="Matthew" />
+            { content }
             
         </div>
 
