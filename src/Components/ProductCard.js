e7a59c3103658b1fc017b1ad71941ba3b0fc5526
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-10 19:12:13 -0600

    updated

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 function ProductCard(props) {
     return(
-        <div>
-            { props.product.name }
-            { props.product.price }
+        <div className="border mb-4 rounded overflow-hidden">
+           <div
+            style={{
+                'backgroundImage': `url('${props.product.imageUrl[0].imageUrl}')`
+            }}
+            className="w-full h-64 bg-blue bg-cover"
+
+           >
+
+           </div>
         </div>
 
     )
