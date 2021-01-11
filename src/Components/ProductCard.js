24838f9ddeee47f4a6e19443da747bcb1a6caa5d
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-10 19:42:54 -0600

    updated

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ProductCard(props) {
     return(
         <div className="border mb-4 rounded overflow-hidden">
+            <Link to={`/PRODUCTS/${props.product.id}`}>
            <div
             style={{
                 'backgroundImage': `url('${props.product.imageUrl[0].imageUrl}')`
             }}
-            className="w-full h-64 bg-blue bg-cover"
-
-           >
-
+            className="w-full h-64 bg-blue bg-cover">
+           </div>
+           </Link>
+           <div className="p-3">
+            <h3 className="font-bold text-xl mb-3">
+                <Link to={`/PRODUCTS/${props.product.id}`}>
+                    { props.product.name }
+                </Link>
+            </h3>
            </div>
         </div>
 
