de362b3aae02fa732b7b948b156d4ba4b171072f
Matt Murphy <murphy.d.matthew28@gmail.com>
2021-01-10 22:16:58 -0600

    updated

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navigation from './Navigation'
 
 
@@ -6,10 +7,11 @@ function Header() {
 return(
     <header className="border-b p-3 flex justify-between items-center">
 
+                <Link to='/'>
             <span className="font-bold">
                 App Name
             </span>
-     
+            </Link>
 
         <Navigation />
 
